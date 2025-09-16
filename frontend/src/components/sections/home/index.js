49a25f2656d6a7ccad441ce00cca8909dff42e4a
Jeremy Garin <152013192+emperuna@users.noreply.github.com>
2025-09-16 15:52:54 +0800

    feat: Add common UI components including Button, Input, Loading, Navbar, and Footer

---
+++ frontend/src/components/sections/home/index.js
@@ -0,0 +1,5 @@
+export { default as Hero } from './Hero'
+export { default as Features } from './Features'
+export { default as CTA } from './CTA'
+export { default as JobSearch } from './JobSearch'
+export { default as ScrollCard } from './ScrollCard'
