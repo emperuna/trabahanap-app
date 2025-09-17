a0fc67a057ff85994bb67968e504a3cd158c3bb7
Jeremy Garin <152013192+emperuna@users.noreply.github.com>
2025-09-17 16:50:48 +0800

    feat: Integrate Chakra UI provider and update rendering structure in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import App from './App.jsx'
+import theme from './theme' // <-- Import your theme
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config?.initialColorMode || 'light'} />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
 )
