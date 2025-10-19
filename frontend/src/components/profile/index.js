f9cbd94f5976ecf0158798cf968b319198955fb8
emperuna <garinjeremy6@gmail.com>
2025-10-19 14:01:38 +0800

    feat: implement Job Seeker Profile components and hooks, refactor profile layout

--- frontend/src/components/profile/index.js
+++ frontend/src/components/profile/index.js
@@ -1,12 +1,5 @@
 // Job Seeker Profile Components
-export { default as AboutMe } from './jobseeker/AboutMe';
-export { default as Contact } from './jobseeker/Contact';
-export { default as Experience } from './jobseeker/Experience';
-export { default as Portfolio } from './jobseeker/Portfolio';
-export { default as ProfileHeader } from './jobseeker/ProfileHeader';
-export { default as ProfileQuickActions } from './jobseeker/ProfileQuickActions';
-export { default as Skills } from './jobseeker/Skills';
-export { default as ResumeUpload } from './jobseeker/ResumeUpload';
+export * from './jobseeker';
 
 // Employer Profile Components (when you create them)
 // export * from './employer';
